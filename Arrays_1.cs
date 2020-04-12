063977312c0b8a5c28c3631930019ce461204e7f
Dylan Dinsdale <101669460@student.swi.edu.au>
2020-04-12 13:33:51 +1000

    commit

---
+++ Arrays_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lists_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] names = new string[5];
+
+            for(int i = 0; i < names.Length; i++){
+                string name=Console.ReadLine();
+                names[i] = name;
+            }
+
+            Console.WriteLine("================================");
+
+            for(int i = 0; i < names.Length; i++){
+                Console.WriteLine("Hello " + names[i]);
+            }
+
+        }
+    }
+}
